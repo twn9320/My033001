816c5741879020856687b887d927351e9d304454
twn9320 <twn9320@gmail.com>
2017-03-30 10:31:36 +0800

    點下選項 用toast顯示所按的選項

--- app/src/main/java/com/example/yvtc/my033001/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my033001/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.yvtc.my033001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
@@ -16,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(MainActivity.this,
                              android.R.layout.simple_list_item_1,fruits);
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"你選了"+fruits[position],Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
